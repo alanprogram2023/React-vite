61f11a06517cbda33f934d65736972d90e0d294c
alanprogram2023 <alanprogram2023@gmail.com>
2023-04-08 21:05:52 -0300

    nuevo

---
+++ projects/00-Hola-Mundo/src/App1.jsx
@@ -0,0 +1,16 @@
+export function App1 () {
+    return (
+        <article>
+            <header>
+                <img alt="El avatar de Miguel"src="https://cdn-icons-png.flaticon.com/512/147/147144.png" />
+                <div>
+                    <strong>Miguel Sanchez</strong>
+                    <span>@miguel</span>
+                </div>
+            </header>
+                <aside>
+                    <button>Seguir</button>
+                </aside>
+        </article>   
+    )
+}
